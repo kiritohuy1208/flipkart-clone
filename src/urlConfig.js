cc59bf927f859a8399d00780497ddb155776be43
Tô Huy <kiritohuy1208>
2021-08-20 15:31:07 +0700

    fix urlconfig

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,10 @@
-export const api = 'http://localhost:8080/api'
-export const generatePublicUrl = (filename) =>{
-    return `http://localhost:8080/public/${filename}`
-}
+// export const api = 'http://localhost:8080/api'
+// export const generatePublicUrl = (filename) =>{
+//     return `http://localhost:8080/public/${filename}`
+// }
+const baseUrl = "https://flipkart-rest-backend.herokuapp.com";
+
+export const api = `${baseUrl}/api`;
+export const generatePublicUrl = (filename) => {
+  return `${baseUrl}/public/${filename}`;
+};
